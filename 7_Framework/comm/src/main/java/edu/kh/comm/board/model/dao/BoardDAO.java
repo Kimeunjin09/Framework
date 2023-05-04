bc86b3f335a12fda8d34e51b206e32228acb2522
Kimeunjin09 <rladmswls85@gmail.com>
2023-05-04 15:12:48 +0900

    0504

---
+++ 7_Framework/comm/src/main/java/edu/kh/comm/board/model/dao/BoardDAO.java
@@ -0,0 +1,113 @@
+package edu.kh.comm.board.model.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import edu.kh.comm.board.model.vo.Board;
+import edu.kh.comm.board.model.vo.BoardDetail;
+import edu.kh.comm.board.model.vo.BoardType;
+import edu.kh.comm.board.model.vo.Pagination;
+
+@Repository
+public class BoardDAO {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	/** 게시판 코드, 이름 조회 DAO
+	 * @return boardTypeList
+	 */
+	public List<BoardType> selectBoardType() {
+		
+		return sqlSession.selectList("boardMapper.selectBoardType");
+	}
+
+	/** 특정 게시판의 전체 게시글 수 조회 DAO
+	 * @param boardCode
+	 * @return listCount
+	 */
+	public int getListCount(int boardCode) {
+		return sqlSession.selectOne("boardMapper.getListCount", boardCode);
+	}
+
+	/** 게시판 목록 조회 DAO
+	 * @param pagination
+	 * @param boardCode
+	 * @return boardList
+	 */
+	public List<Board> selectBoardList(Pagination pagination, int boardCode) {
+		
+		// RowBounds 객체 (마이바티스)
+		// - 전체 조회 결과에서
+		//	 몇 개의 행을 건너 뛰고 (offset)
+		//	 그 다음 몇개의 행만(limit) 조회할 것인지 지정
+		
+		int offset = ( pagination.getCurrentPage() - 1 ) * pagination.getLimit();
+		
+		RowBounds rowBounds = new RowBounds(offset, pagination.getLimit());
+		
+		return sqlSession.selectList("boardMapper.selectBoardList", boardCode, rowBounds);
+	}
+
+	/** 게시글 상세조회 DAO
+	 * @param boardNo
+	 * @return detail
+	 */
+	public BoardDetail selectBoardDetail(int boardNo) {
+		
+		return sqlSession.selectOne("boardMapper.selectBoardDetail", boardNo);
+	}
+
+	/** 조회수 증가 DAO
+	 * @param boardNo
+	 * @return result
+	 */
+	public int updateReadCount(int boardNo) {
+		
+		return sqlSession.update("boardMapper.updateReadCount", boardNo);
+	}
+
+	/** 검색 조건에 맞는 게시글 목록의 전체 개수 조회 DAO
+	 * @param paramMap
+	 * @return listCount
+	 */
+	public int searchBoardList(Map<String, Object> paramMap) {
+		
+		return sqlSession.selectOne("boardMapper.searchBoardList", paramMap);
+	}
+
+	/** 검색 조건에 맞는 게시글 목록 조회(페이징 처리 적용)
+	 * @param paramMap
+	 * @param pagination
+	 * @return	boardList
+	 */
+	public List<Board> searchBoardList(Map<String, Object> paramMap, Pagination pagination) {
+		
+		int offset = ( pagination.getCurrentPage() - 1 ) * pagination.getLimit();
+		
+		RowBounds rowBounds = new RowBounds(offset, pagination.getLimit());
+		
+		return sqlSession.selectList("boardMapper.searchBoardList", paramMap, rowBounds);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ 7_Framework/comm/src/main/java/edu/kh/comm/board/model/service/BoardServiceImpl.java
@@ -0,0 +1,101 @@
+package edu.kh.comm.board.model.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.kh.comm.board.model.dao.BoardDAO;
+import edu.kh.comm.board.model.vo.Board;
+import edu.kh.comm.board.model.vo.BoardDetail;
+import edu.kh.comm.board.model.vo.BoardType;
+import edu.kh.comm.board.model.vo.Pagination;
+
+@Service
+public class BoardServiceImpl implements BoardService{
+	
+	@Autowired
+	private BoardDAO dao;
+
+	// 게시판 코드, 이름 조회
+	@Override
+	public List<BoardType> selectBoardType() {
+		
+		return dao.selectBoardType();
+	}
+
+	// 게시글 목록 조회 서비스 구현
+	@Override
+	public Map<String, Object> selectBoardList(int cp, int boardCode) {
+		// 1) 게시판 이름 조회 -> 인터셉터로 application에 올려둔 boardTypeList 쓸 수 있을듯?
+		
+		// 2) 페이지네이션 객체 생성(listCount)
+		int listCount = dao.getListCount(boardCode);
+		Pagination pagination = new Pagination(cp, listCount);
+		
+		
+		// 3) 게시글 목록 조회
+		List<Board> boardList = dao.selectBoardList(pagination, boardCode);
+		
+		// map 만들어 담기
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("pagination", pagination);
+		map.put("boardList", boardList);
+		
+		
+		return map;
+	}
+
+	// 게시글 상세조회 서비스 구현
+	@Override
+	public BoardDetail selectBoardDetail(int boardNo) {
+		
+		return dao.selectBoardDetail(boardNo);
+	}
+
+	// 조회수 증가 서비스 구현
+	@Override
+	public int updateReadCount(int boardNo) {
+		
+		return dao.updateReadCount(boardNo);
+	}
+
+	// 검색 게시글 목록 조회 서비스 구현
+	@Override
+	public Map<String, Object> searchBoardList(Map<String, Object> paramMap) {
+		
+		// 검색 조건에 맞는 게시글 목록의 전체 개수 조회
+		int listCount = dao.searchBoardList(paramMap);
+		
+		// 페이지네이션 객체 생성
+		Pagination pagination = new Pagination( (int)paramMap.get("cp"), listCount);
+		
+		// 검색 조건에 맞는 게시글 목록 조회 (페이징 처리 적용)
+		List<Board> boardList = dao.searchBoardList(paramMap, pagination);
+		
+		
+		// map에 담기
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("pagination", pagination);
+		map.put("boardList", boardList);
+		
+		
+		return map;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
